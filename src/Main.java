7e75ee67492e6d98d3b911027d3a236cae540e76
Kratos410 <fanur.tt64@gmail.com>
2022-12-23 00:09:03 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 2_500; //стоимость билета
+        {
+            System.out.println("Стоимость билета:  " + x );
+        }
+
+        int r = x / 20;
+        {
+            System.out.println("Ваши бонусныемили составляют " + r + "  миль");
+        }
+
+
+    }
+}
