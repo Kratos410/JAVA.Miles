1c7615718c47189c76b41cfc5314230206ba6e69
Kratos410 <fanur.tt64@gmail.com>
2022-12-27 11:40:26 +0300

    fixed variables2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int ticketPrice = 2_500; //стоимость билета
-        {
-            System.out.println("Стоимость билета:  " + x );
-        }
+        System.out.println("Стоимость билета:  " + ticketPrice);
+        int miles = ticketPrice / 20; //Подсчет бонусых миль
+        System.out.println("Ваши бонусы составляют: " + miles + " миль");
 
-        int miles = x / 20; //Подсчет бонусых миль
-        {
-            System.out.println("Ваши бонусы составляют: " + r + " миль");
-        }
 
 
     }
